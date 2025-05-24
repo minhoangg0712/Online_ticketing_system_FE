dc56929839d329a687c2a83943a06224dcd17fab
tranphucnguyen <nguyenhuong1402802004@gmail.com>
2025-05-24 22:14:43 +0700

    Giao diện admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { HomeOrganizerComponent } from './organizer/pages/home-organizer/home-or
 import { NavOrganizerComponent } from './component/nav-organizer/nav-organizer.component';
 import { ExportFileComponent } from './organizer/pages/export-file/export-file.component';
 import { HomeUserComponent } from './user/pages/home-user/home-user.component';
-
+import { HomeAdminComponent } from './admin/pages/home-admin/home-admin.component';
 export const routes: Routes = [
-    {
+  {
     path: 'organizer',
     component: NavOrganizerComponent,
     children: [
@@ -13,5 +13,12 @@ export const routes: Routes = [
       { path: 'export-file', component: ExportFileComponent },
     ]
   },
-  {path: 'home', component: HomeUserComponent}
+  { path: 'home', component: HomeUserComponent },
+  {
+    path: 'admin',
+    component: HomeAdminComponent,
+    // children: [
+    //   { path: 'home', component: HomeAdminComponent },
+    // ]
+  }
 ];
