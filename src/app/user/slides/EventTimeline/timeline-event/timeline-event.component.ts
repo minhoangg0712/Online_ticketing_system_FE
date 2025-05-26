2d2f4c6e01784be5d1e0d7550982f248d6d96ad2
dukesodope <ductrantrung10@gmail.com>
2025-05-26 23:29:32 +0700

    sua slide danh cho ban,them slide timeline

---
+++ src/app/user/slides/EventTimeline/timeline-event/timeline-event.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ThismonthEventComponent } from '../tabs/thismonth-event/thismonth-event.component';
+import { ThisweekendEventComponent } from '../tabs/thisweekend-event/thisweekend-event.component';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-timeline-event',
+  imports: [ThismonthEventComponent,ThisweekendEventComponent, CommonModule],
+  templateUrl: './timeline-event.component.html',
+  styleUrl: './timeline-event.component.css'
+})
+export class TimelineEventComponent {
+
+  selectedTab: string = 'thisweekend';
+  setTab(tab: string) {
+    this.selectedTab = tab;
+  }
+
+  onSeeMore() {
+    // điều hướng hoặc hiển thị thêm nội dung
+    console.log('Xem thêm được bấm');
+  }
+
+}
