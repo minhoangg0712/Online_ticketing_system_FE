232073f7cf1b4e922f43bbf490e020457a0fb778
dukesodope <ductrantrung10@gmail.com>
2025-06-12 16:27:14 +0700

    chinh lai purchased-ticket,them cac tab all,success,processing va cancel

--- src/app/user/pages/purchased-ticket/purchased-ticket.component.ts
+++ src/app/user/pages/purchased-ticket/purchased-ticket.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavUserComponent } from '../../../component/nav-user/nav-user.component';
 import { CommonModule } from '@angular/common';
+import { AllComponent } from '../tabs/all/all.component';
+import { CancelComponent } from '../tabs/cancel/cancel.component';
+import { ProcessingComponent } from '../tabs/processing/processing.component';
+import { SuccessComponent } from '../tabs/success/success.component';
 
 @Component({
   selector: 'app-purchased-ticket',
-  imports: [ NavUserComponent, CommonModule ],
+  imports: [ NavUserComponent, CommonModule, AllComponent, CancelComponent, ProcessingComponent, SuccessComponent ],
   templateUrl: './purchased-ticket.component.html',
   styleUrl: './purchased-ticket.component.css'
 })
-export class PurchasedTicketComponent {
+export class PurchasedTicketComponent implements OnInit {
   selectedTab: string = 'all';
 
   setTab(tab: string) {
     this.selectedTab = tab;
   }
+
+  constructor() { }
+
+  ngOnInit(): void {}
 }
---
+++ src/app/user/pages/tabs/cancel/cancel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cancel',
+  imports: [],
+  templateUrl: './cancel.component.html',
+  styleUrl: './cancel.component.css'
+})
+export class CancelComponent {
+
+}
